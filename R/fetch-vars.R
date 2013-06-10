5448306703cda9e7657d4f00e28a7c19b4e5b188
Johann de Boer <johanndeboer@gmail.com>
2013-06-10 20:25:28 +1000

    Adding comments and code clean up

--- R/fetch-vars.R
+++ R/fetch-vars.R
@@ -3,15 +3,25 @@ library(XML)
 library(plyr)
 library(stringr)
 
+# At present the Dims and Mets reference guide does not mention
+# the isTablet dimension.
+
+# The home page URL of Dimensions and Metrics reference guide
 base_url <- "https://developers.google.com/analytics/devguides/reporting/core/dimsmets"
+# Get the home page, which contains an index of all dimensions
+# and metrics. Parse as an XML document for scraping.
 res <- GET(base_url)
 pars <- htmlTreeParse(res, useInternalNodes = TRUE)
 
+# XPath for the index table of all dims and mets
 table_xpath <- "//*[@id=\"table\"]"
+# There is only one index table, so grab the first one.
 table_node <- getNodeSet(pars, table_xpath)[[1]]
 
+# The categories and the URL to details about each category.
 cats_xpath <- "./*[@class=\"cats\"]/a"
 cats_nodeSet <- getNodeSet(table_node, cats_xpath)
+# Build a table of category pages and their corresponding URLs
 cats <- ldply(cats_nodeSet, function(node){
   data.frame(
     cat = xmlValue(node),
@@ -20,9 +30,11 @@ cats <- ldply(cats_nodeSet, function(node){
   )
 })
 
+# Table of all dimensions and metrics and for each category.
 entity_table_xpath <- "./*[@class=\"entity table\"]"
 entity_table_nodeSet <- getNodeSet(table_node, entity_table_xpath)
 
+# Extract a list of dimensions and metrics separately for each category.
 dims_xpath <- "./*[@class=\"dim\"]/*/a"
 mets_xpath <- "./*[@class=\"met\"]/*/a"
 entities <- ldply(entity_table_nodeSet, function(node){
@@ -37,7 +49,6 @@ entities <- ldply(entity_table_nodeSet, function(node){
     href <- str_split_fixed(df$href, "#", 2)
     df$href <- NULL
     df$page <- href[,1]
-    #df$anchor <- href[,2]
     df
   })
   mets_nodeSet <- getNodeSet(node, mets_xpath)
@@ -51,12 +62,12 @@ entities <- ldply(entity_table_nodeSet, function(node){
     href <- str_split_fixed(df$href, "#", 2)
     df$href <- NULL
     df$page <- href[,1]
-    #df$anchor <- href[,2]
     df
   })
   entity_table <- rbind(dims, mets)
 })
 
+# Join the table of variables with details about each category
 entities <- join(entities, cats, "page")
 entities <- mutate(
   entities,
@@ -65,7 +76,9 @@ entities <- mutate(
   cat = factor(cat)
 )
 
-entities2 <- ddply(entities, "page", function(page_entities){
+# Get the descriptions for each dimension and metric along
+# with indicator of whether a metric is calculated.
+entities <- ddply(entities, "page", function(page_entities){
   url <- paste(base_url, unique(page_entities$page), sep = "/")
   res <- GET(url)
   pars <- htmlTreeParse(res, useInternalNodes = TRUE)
@@ -88,8 +101,6 @@ entities2 <- ddply(entities, "page", function(page_entities){
   }
   page_entities$description <- descriptions[page_entities$var]
   page_entities
-}
-, .progress = "time"
-)
+}, .progress = "time")
 
-write.csv(entities2, file = "~/gavars.csv")
+write.csv(entities, file = "~/gavars.csv")
