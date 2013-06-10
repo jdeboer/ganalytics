399719c0b8785093ffbe0736c952fa8b7f7c3a5b
Johann de Boer <johanndeboer@gmail.com>
2013-06-10 21:04:32 +1000

    Code clean up and simplifications

--- R/fetch-vars.R
+++ R/fetch-vars.R
@@ -34,37 +34,28 @@ cats <- ldply(cats_nodeSet, function(node){
 entity_table_xpath <- "./*[@class=\"entity table\"]"
 entity_table_nodeSet <- getNodeSet(table_node, entity_table_xpath)
 
+GetVars <- function(node, type) {
+  xpath <- paste("./*[@class=", type, "]/*/a", sep = "\"")
+  nodeSet <- getNodeSet(node, xpath)
+  ldply(nodeSet, function(node){
+    df <- data.frame(
+      var = xmlValue(node),
+      type = type,
+      page = str_replace(xmlGetAttr(node, "href"), ".*/", ""),
+      stringsAsFactors = FALSE
+    )
+    page <- str_split_fixed(df$page, "#", 2)
+    df$page <- page[,1]
+    df
+  })
+}
+
 # Extract a list of dimensions and metrics separately for each category.
-dims_xpath <- "./*[@class=\"dim\"]/*/a"
-mets_xpath <- "./*[@class=\"met\"]/*/a"
+
 entities <- ldply(entity_table_nodeSet, function(node){
-  dims_nodeSet <- getNodeSet(node, dims_xpath)
-  dims <- ldply(dims_nodeSet, function(node){
-    df <- data.frame(
-      var = xmlValue(node),
-      type = "dim",
-      href = str_replace(xmlGetAttr(node, "href"), ".*/", ""),
-      stringsAsFactors = FALSE
-    )
-    href <- str_split_fixed(df$href, "#", 2)
-    df$href <- NULL
-    df$page <- href[,1]
-    df
-  })
-  mets_nodeSet <- getNodeSet(node, mets_xpath)
-  mets <- ldply(mets_nodeSet, function(node){
-    df <- data.frame(
-      var = xmlValue(node),
-      type = "met",
-      href = str_replace(xmlGetAttr(node, "href"), ".*/", ""),
-      stringsAsFactors = FALSE
-    )
-    href <- str_split_fixed(df$href, "#", 2)
-    df$href <- NULL
-    df$page <- href[,1]
-    df
-  })
-  entity_table <- rbind(dims, mets)
+  dims <- GetVars(node, "dim")
+  mets <- GetVars(node, "met")
+  rbind(dims, mets)
 })
 
 # Join the table of variables with details about each category
@@ -82,24 +73,41 @@ entities <- ddply(entities, "page", function(page_entities){
   url <- paste(base_url, unique(page_entities$page), sep = "/")
   res <- GET(url)
   pars <- htmlTreeParse(res, useInternalNodes = TRUE)
+  
+  # Get the main content of the category page
   cat_xpath <- "//div[@itemprop = \"articleBody\"]"
   cat_node <- getNodeSet(pars, cat_xpath)[[1]]
+  
+  # Get the names of dim/met names for each description given
   desc_name_xpath <- "//h3[starts-with(@id, \"ga:\")]/text()"
   desc_name_nodes <- getNodeSet(cat_node, desc_name_xpath)
   desc_names <- xmlSApply(desc_name_nodes, xmlValue)
+  
+  # Get the description that follows each dim/met name, removing
+  # new lines, leading and consecutive spaces.
   desc_xpath <- "//h3[starts-with(@id, \"ga:\")]/following-sibling::div"
   desc_nodes <- getNodeSet(cat_node, desc_xpath)
   descriptions <- str_trim(xmlSApply(desc_nodes, xmlValue))
   descriptions <- str_replace_all(descriptions, "\\n", " ")
   descriptions <- str_replace_all(descriptions, "[ ]{2,}", " ")
+  
+  # Apply the names to each of the descriptions and add the matching
+  # the descriptions to the data.frame.
   names(descriptions) <- desc_names
+  page_entities$description <- descriptions[page_entities$var]
+  
+  # Identify calculated metrics
   calc_xpath <- "//*[@href = \"#calc\"]/following-sibling::*//a/text()"
   calc_metrics <- xpathSApply(cat_node, calc_xpath, xmlValue)
-  page_entities$type <- as.character(page_entities$type)
+  
+  # If any calculated metrics
   if(length(calc_metrics) > 0) {
+    page_entities$type <- as.character(page_entities$type)
     page_entities[page_entities$var %in% calc_metrics, 'type'] <- "cal"
+    page_entities$type <- factor(page_entities$type)
   }
-  page_entities$description <- descriptions[page_entities$var]
+  
+  # Return the resulting data.frame for that particular category page
   page_entities
 }, .progress = "time")
 
