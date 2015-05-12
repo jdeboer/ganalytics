f3b7bb750ed90d645f77edc4dd0cb67f51be337a
Johann de Boer <johann@lovesdata.com>
2015-05-12 12:01:20 +1000

    Adding httpuv dependency

--- R/ganalytics-package.R
+++ R/ganalytics-package.R
@@ -1,17 +1,18 @@
 #' ganalytics
-#' 
+#'
 #' A Google Analytics client for R
-#' 
+#'
 #' S4 classes and methods for interacting with Google Analytics data in R.
-#' 
+#'
 #' @references Google Analytics dimensions and metrics reference:
 #'   \url{https://developers.google.com/analytics/devguides/reporting/core/dimsmets}
-#'   
+#'
 #' @keywords package
+#' @import httpuv
 #' @importFrom R6 R6Class
 #' @importFrom lubridate now ymd_hms
 #' @importFrom selectr querySelector querySelectorAll
-#' @importFrom httr oauth_endpoints oauth1.0_token oauth2.0_token config 
+#' @importFrom httr oauth_endpoints oauth1.0_token oauth2.0_token config
 #'   stop_for_status content oauth_app modify_url add_headers GET POST PUT
 #'   DELETE
 #' @importFrom stringr str_c str_detect str_replace_all str_replace
