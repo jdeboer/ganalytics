43ba9490e23d5ddd7371466efeb754356f863e66
Johann de Boer <johann@lovesdata.com>
2015-08-28 13:45:16 +1000

    Enhance user support messages about deprecated functions.

--- R/backwards-compatibility.R
+++ R/backwards-compatibility.R
@@ -4,75 +4,111 @@
 #' @export GaNot
 #' @param ... arguments passed to \code{Not}
 #' @rdname GaNot
-GaNot <- function(...){Not(...)}
+GaNot <- function(...){
+  .Deprecated("Not")
+  Not(...)
+}
 
 #' GaOr (Deprecated, use Or or | instead).
 #' @export GaOr
 #' @param ... arguments passed to \code{Or}
 #' @rdname GaOr
-GaOr <- function(...){Or(...)}
+GaOr <- function(...){
+  .Deprecated("Or")
+  Or(...)
+}
 
 #' GaAnd (Deprecated, use And or & instead).
 #' @export GaAnd
 #' @param ... arguments passed to \code{And}
 #' @rdname GaAnd
-GaAnd <- function(...){And(...)}
+GaAnd <- function(...){
+  .Deprecated("And")
+  And(...)
+}
 
 #' GaStartDate (Deprecated)
 #' For backwards compatibility
 #' @export GaStartDate
 #' @rdname GaStartDate
 #' @param ... arguments passed onto \code{StartDate}
-GaStartDate <- function(...){StartDate(...)}
+GaStartDate <- function(...){
+  .Deprecated("StartDate")
+  StartDate(...)
+}
 
 #' GaEndDate (Deprecated)
 #' @export GaEndDate
 #' @rdname GaEndDate
 #' @param ... arguments passed onto \code{EndDate}
-GaEndDate <- function(...){EndDate(...)}
+GaEndDate <- function(...){
+  .Deprecated("EndDate")
+  EndDate(...)
+}
 
 #' GaDateRange (Deprecated)
 #' @export GaDateRange
 #' @rdname GaDateRange
 #' @param ... arguments passed onto \code{DateRange}
-GaDateRange <- function(...){DateRange(...)}
+GaDateRange <- function(...){
+  .Deprecated("DateRange")
+  DateRange(...)
+}
 
 #' GaStartDate<- (Deprecated)
 #' @export GaStartDate<-
 #' @rdname GaStartDate
 #' @param value passed onto \code{StartDate}
-`GaStartDate<-` <- function(..., value){`StartDate<-`(..., value)}
+`GaStartDate<-` <- function(..., value){
+  .Deprecated("StartDate<-")
+  `StartDate<-`(..., value)
+}
 
 #' GaEndDate<- (Deprecated)
 #' @export GaEndDate<-
 #' @rdname GaEndDate
 #' @param value passed onto \code{EndDate}
-`GaEndDate<-` <- function(..., value){`EndDate<-`(..., value)}
+`GaEndDate<-` <- function(..., value){
+  .Deprecated("EndDate<-")
+  `EndDate<-`(..., value)
+}
 
 #' GaDateRange<- (Deprecated)
 #' @export GaDateRange<-
 #' @rdname GaDateRange
 #' @param value passed onto \code{DateRange}
-`GaDateRange<-` <- function(..., value){`DateRange<-`(..., value)}
+`GaDateRange<-` <- function(..., value){
+  .Deprecated("DateRange<-")
+  `DateRange<-`(..., value)
+}
 
 #' GaSplitDateRange (Deprecated)
 #' @export GaSplitDateRange
 #' @param ... arguments passed onto \code{SplitDateRange}
-GaSplitDateRange <- function(...){SplitDateRange(...)}
+GaSplitDateRange <- function(...){
+  .Deprecated("SplitDateRange")
+  SplitDateRange(...)
+}
 
 #' GaSegment (Deprecated).
 #'
 #' @param ... arguments passed onto \code{Segment}
 #' @export GaSegment
 #' @rdname GaSegment
-GaSegment <- function(...){Segment(...)}
+GaSegment <- function(...){
+  .Deprecated("Segment")
+  Segment(...)
+}
 
 #' GaSegment<- (Deprecated).
 #'
 #' @param value passed onto \code{Segment}
 #' @export GaSegment<-
 #' @rdname GaSegment
-`GaSegment<-` <- function(..., value){`Segment<-`(..., value)}
+`GaSegment<-` <- function(..., value){
+  .Deprecated("Segment<-")
+  `Segment<-`(..., value)
+}
 
 #' @export GaFilter
 #' @rdname TableFilter
