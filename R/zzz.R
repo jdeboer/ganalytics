4281437117403b342c80ed262626da2e2750d254
Johann de Boer <johann@lovesdata.com>
2015-08-28 13:46:40 +1000

    Update package startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,6 @@
+#' @include backwards-compatibility.R
+NULL
+
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage('Welcome to ganalytics.\n\nTo get started, use the GoogleApiCreds() function to set credentials for authenticating with your Google Analytics API project. E.g:\n creds <- GoogleApiCreds("<my_user_name>@gmail.com", ".my_app_oauth_creds.json") \n\nThen, check out the list of available demos with:\n demo(package = "ganalytics")\n\nFor help with authentication, enter:\n ?ganalytics::GoogleApiCreds')
+  packageStartupMessage('Welcome to ganalytics.\n\nTo get started, use the GoogleApiCreds() function to set credentials for authenticating with your Google Analytics API project. E.g:\n creds <- GoogleApiCreds("<my_user_name>@gmail.com", ".my_app_oauth_creds.json") \n\nThen, check out the list of available demos and package documentation with:\n package?ganalytics\n demo(package = "ganalytics")\n\nFor help with authentication, enter:\n ?ganalytics::GoogleApiCreds')
 }
