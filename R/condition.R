ab0bc73dc0f32df1015682f428466e4d3d58f2a3
Johann de Boer <johann@lovesdata.com>
2015-08-30 20:06:47 +1000

    Add new condition function for defining expressions using a domain specific language that is more familiar to R users, with the use of non-standard evaluation.

---
+++ R/condition.R
@@ -0,0 +1,29 @@
+#' @importFrom lazyeval lazy
+NULL
+
+#' condition
+#'
+#' Define a Google Analytics condition using a domain-specific language familiar to R users and using non-standard evaluation.
+#'
+#' @export
+condition <- function(cond) {
+  lazy_expr <- lazy(cond)
+  assert_that(is.call(lazy_expr$expr))
+  assert_that(length(lazy_expr$expr) == 3)
+  comparator <- as.character(lazy_expr$expr[[1]])
+  comparator <- switch(
+    comparator,
+    `%starts_with%` = "=@",
+    `=@` = "=@",
+    `%matches%` = "=~",
+    `%=~%` = "=~",
+    `%in%` = "[]",
+    `%[]%` = "[]",
+    `%between%` = "<>",
+    `%<>%` = "<>",
+    comparator
+  )
+  var <- as.character(lazy_expr$expr[[2]])
+  operand <- lazy_expr$expr[[3]]
+  Expr(var, comparator, eval(operand, envir = lazy_expr$env))
+}
