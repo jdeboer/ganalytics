d6a4d4ada126cb5464410c987dcf5ea52b34e3a4
Johann de Boer <johann@lovesdata.com>
2015-09-18 14:14:32 +1000

    Complete implementation of sequential_segment function that generates a sequential segment expression by applying NSE to its argument.

--- R/sequence.R
+++ R/sequence.R
@@ -4,10 +4,11 @@ NULL
 
 #' sequential_segment.
 #'
-#' Create a sequence using non-standard evaluation.
+#' Create a sequence using R non-standard evaluation syntax.
 #'
-#' Steps must be separated by commas (\code{,}). ... denotes zero or more of any
-#' interaction preceeding the step that follows.
+#' Steps must be separated by commas (\code{,}). ... denotes zero or more of
+#' interactions may preceed the step that follows, otherwise without ... then
+#' the there must not be any interactions between the adjacent steps.
 #'
 #' @param steps a list of expressions (of one or more conditions), each
 #'   repreating a step in the sequence.
@@ -23,7 +24,12 @@ NULL
 sequential_segment <- function(steps){
   lazy_expr <- lazy(steps)
   assert_that(lazy_expr$expr[[1]] == "list")
-  step_exprs <- lazy_expr$expr[-1]
-  lapply(step_exprs, as.character)
+  step_exprs <- as.list(lazy_expr$expr[-1])
+  dots <- sapply(step_exprs, function(expr) {any(as.character(expr) == "...")})
+  laters <- c(TRUE, dots[-length(dots)])
+  thens <- !(dots | laters)
+  step_exprs[laters] <- lapply(step_exprs[laters], function(expr) {Later(eval(expr, envir = lazy_expr$env))})
+  step_exprs[thens] <- lapply(step_exprs[thens], function(expr) {Then(eval(expr, envir = lazy_expr$env))})
+  do.call(Sequence, step_exprs[thens | laters])
 }
 
