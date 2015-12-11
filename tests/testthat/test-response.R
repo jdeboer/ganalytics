b05abe6dbeaf2b80e10670cce48ad96283cf63a6
Johann de Boer <johann@lovesdata.com>
2015-12-11 22:51:06 +1100

    Add function to check if API can be used, and tests using API calls.

--- tests/testthat/test-response.R
+++ tests/testthat/test-response.R
@@ -3,3 +3,12 @@ context("Parsing Google Analytics Core Reporting API responses")
 test_that("", {
 
 })
+
+test_that("A basic query can be executed against the Google Store GA account", {
+  check_api()
+  q <- GaQuery(view = "1174")
+  results <- GetGaData(q)
+  expect_is(results, "data.frame")
+  expect_equivalent(dim(results), c(8, 2))
+  expect_named(results, c("date", "sessions"))
+  expect_equivalent(sapply(results, class), c("Date", "numeric"))
