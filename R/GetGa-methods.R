0f384b3f59159e3bedc1367cd41627ecdcae61d6
Johann de Boer <johann@lovesdata.com>
2015-12-12 12:00:09 +1100

    Add ability to query more than 10 metrics in a query.

--- R/GetGa-methods.R
+++ R/GetGa-methods.R
@@ -3,7 +3,7 @@
 #' @include ga-api-coerce.R
 #' @include GaGetCoreReport.R
 #' @importFrom methods setMethod as
-#' @importFrom plyr alply ldply mutate llply laply
+#' @importFrom plyr alply ldply mutate llply laply join
 #' @importFrom scales percent
 NULL
 
@@ -31,46 +31,83 @@ setMethod("GetGaData", ".query", function(
   if (!missing(use_oob)) {
     warning("Argument 'use_oob' is defunct, please use the GaCreds or GoogleApiCreds functions instead to either supply a creds argument or to set the creds of the supplied query object.", call. = FALSE)
   }
-  queryParams <- as(query, "matrix")
+
+  metrics <- as.character(Metrics(query))
+
+  group_metrics <- function(metrics) {
+    split(metrics, ceiling(seq_along(metrics) / kGaMax$metrics))
+  }
+
   if (class(query) == "gaQuery") {
     segmentNames <- names(query@segments)
   } else {
     segmentNames <- NULL
   }
-  # Need to determine if the query object is a MCF or GA query and tell GaPaginate
-  responses <- llply(
-    .data = seq_len(ncol(queryParams)),
-    .fun = function(i) {
-      GaPaginate(
-        queryParams[, i],
-        maxRequestedRows = MaxResults(query),
-        creds = creds,
-        queryClass = class(query),
-        segmentName = segmentNames[i]
-      )
-    },
-    .progress = .progress
-  )
-  data <- ldply(
-    .data = responses,
-    .fun = function(response) {
-      df <- response$data
-      if (addViewId & nrow(df) >= 1) {
-        df <- mutate(df, viewId = response$viewId)
-      }
-      if (addSegmentId & nrow(df) >= 1) {
-        df <- mutate(df, segment = response$segmentName)
-      }
-      return(df)
-    }
-  )
-  attr(data, "sampleSize") <- sum(laply(responses, function(response){as.numeric(response$sampleSize)}))
-  attr(data, "sampleSpace") <- sum(laply(responses, function(response){as.numeric(response$sampleSpace)}))
-  sampleSize <- attr(data, "sampleSize")
-  sampleSpace <- attr(data, "sampleSpace")
-  sampled <- any(laply(responses, function(response) {isTRUE(response$sampled)}))
-  if (sampled) {
-    warning(paste("Contains sampled data: ", sampleSize, "/", sampleSpace, "(", percent(sampleSize/sampleSpace), ")."))
+
+  if (length(segmentNames) == 0) {
+    addSegmentId <- FALSE
   }
-  return(data)
+
+  data_by_metric_group <- lapply(group_metrics(metrics), function(metrics_group) {
+    Metrics(query) <- metrics_group
+    queryParams <- as(query, "matrix")
+
+    # Need to determine if the query object is a MCF or GA query and tell GaPaginate
+    responses <- llply(
+      .data = seq_len(ncol(queryParams)),
+      .fun = function(i) {
+        GaPaginate(
+          queryParams[, i],
+          maxRequestedRows = MaxResults(query),
+          creds = creds,
+          queryClass = class(query),
+          segmentName = segmentNames[i]
+        )
+      },
+      .progress = .progress
+    )
+
+    data <- ldply(
+      .data = responses,
+      .fun = function(response) {
+        df <- response$data
+        if (addViewId & nrow(df) >= 1) {
+          df <- mutate(df, viewId = response$viewId)
+        }
+        if (addSegmentId & nrow(df) >= 1) {
+          df <- mutate(df, segment = response$segmentName)
+        }
+        return(df)
+      }
+    )
+    attr(data, "sampleSize") <- sum(laply(responses, function(response){as.numeric(response$sampleSize)}))
+    attr(data, "sampleSpace") <- sum(laply(responses, function(response){as.numeric(response$sampleSpace)}))
+    sampleSize <- attr(data, "sampleSize")
+    sampleSpace <- attr(data, "sampleSpace")
+    sampled <- any(laply(responses, function(response) {isTRUE(response$sampled)}))
+    if (sampled) {
+      warning(paste("Contains sampled data: ", sampleSize, "/", sampleSpace, "(", percent(sampleSize/sampleSpace), ")."))
+    }
+    data
+  })
+
+  join_by_vars <- c("viewId"[addViewId], "segment"[addSegmentId], sub("^ga:", "", as.character(Dimensions(query))))
+
+  if (length(join_by_vars) == 0) {
+    data <- Reduce(function(x, y) {cbind(x, y)}, data_by_metric_group)
+  } else {
+    data <- Reduce(function(x, y) {join(x, y, by = join_by_vars, type = "full")}, data_by_metric_group)
+  }
+
+  metric_columns <- sub("^ga:", "", metrics)
+
+  data[metric_columns] <- lapply(data[metric_columns], function(col) {
+    col[is.na(col)] <- 0
+    col
+  })
+
+  dim_cols <- colnames(data) %in% join_by_vars
+
+  data[c(which(dim_cols), which(!dim_cols))]
+
 })
