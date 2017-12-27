384741e449683eb40f6b6f2742491c998a4ecae6
Johann de Boer <johanndeboer@gmail.com>
2017-12-28 09:51:48 +1100

    Finish merge

--- R/GaView-methods.R
+++ R/GaView-methods.R
@@ -5,6 +5,46 @@
 #' @importFrom methods setMethod as as<-
 NULL
 
+#######\/ Consider changing to generic methods View and View<- rather than coercion
+setAs(from = ".query", to = "viewId",
+      def = function(from, to) {
+        from@viewId
+      },
+      replace = function(from, value) {
+        from@viewId <- as(value, "viewId")
+        validObject(from)
+        from
+      }
+)
+
+#######\/ Consider changing to generic methods rather than coercion
+# Select the default view of the property
+setAs(from = "gaProperty", to = "viewId",
+      def = function(from, to) {
+        defaultView <- from$defaultView
+        if (length(defaultView) == 0) {
+          defaultView <- from$views$entities[[1]]
+        }
+        as(defaultView, "viewId")
+      },
+      replace = function(from, value) {
+        as(from$defaultView, "viewId") <- as(value, "viewId")
+      }
+)
+
+#######\/ Consider changing to generic methods rather than coercion
+# Sselect the first property of the account, which is then
+# used to select a view (as above).
+setAs(from = "gaAccount", to = "viewId",
+      def = function(from, to) {
+        as(from$properties$entities[[1]], "viewId")
+      },
+      replace = function(from, value) {
+        as(from$properties$entities[[1]], "viewId") <- as(value, "viewId")
+      }
+)
+
+# -- GaView ----
 #' @describeIn GaView Returns the ID of the supplied view, or the first view
 #'   within the supplied property or the first view within the first property of
 #'   the supplied account, or gets the view ID of the supplied query, or coerce a
@@ -30,3 +70,12 @@ setMethod(f = "GaView<-", signature = c(".query", "ANY"),
           }
         )
 
+#' @describeIn GaView Replaces the view being used by a query.
+setMethod(
+  f = "GaView<-",
+  signature = c(".query", "ANY"),
+  definition = function(object, value) {
+    as(object, "viewId") <- value
+    object
+  }
+)
