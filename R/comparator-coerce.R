f226399a798c563b1de16cb66e94a1d7e3a52330
Johann de Boer <johanndeboer@gmail.com>
2018-01-06 23:37:19 +1100

    Add comparator coercion methods

--- R/comparator-coerce.R
+++ R/comparator-coerce.R
@@ -9,3 +9,21 @@ setAs(from = "character", to = "mcfDimComparator", def = simpleCoerce)
 setAs(from = "character", to = "mcfMetComparator", def = simpleCoerce)
 setAs(from = "character", to = "rtDimComparator", def = simpleCoerce)
 setAs(from = "character", to = "rtMetComparator", def = simpleCoerce)
+setAs(
+  from = "gaDimComparator", to = "character",
+  def = function(from, to) {
+    as.character(from)
+  },
+  replace = function(from, value) {
+    new(class(from), value)
+  }
+)
+setAs(
+  from = "gaMetComparator", to = "character",
+  def = function(from, to) {
+    as.character(from)
+  },
+  replace = function(from, value) {
+    new(class(from), value)
+  }
+)
