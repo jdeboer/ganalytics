8b5f73fef00c2be3230236d37c205b4fe1ff1062
Johann de Boer <johanndeboer@gmail.com>
2018-05-30 21:11:16 +1000

    Extend coercion methods for googleAnalyticsR

--- demo/googleAnalyticsR_demo.R
+++ demo/googleAnalyticsR_demo.R
@@ -4,20 +4,31 @@ library(googleAnalyticsR)
 my_segment_list <- list(
   bounced_sessions = PerSession(Expr(~bounces != 0)),
   from_google = Expr(~source == "google"),
-  repeat_user_sessions = PerUser(Expr(~sessions > 1))
+  repeat_user_sessions = Include(PerUser(Expr(~sessions > 1)), scope = "users")#,
+#  simple_segment = Segment(
+#    Include(PerUser(Expr(~sessions > 1)), scope = "users"),
+#    Include(Expr(~medium == "organic"), scope = "users")
+#  )
 )
-
 my_segment_list <- lapply(names(my_segment_list), function(segment_name) {
-  segment_def <- my_segment_list[[segment_name]]
+  my_segment <- as(my_segment_list[[segment_name]], ".gaSegmentFilter")
   segment_def <- as(
-    SegmentConditionFilter(segment_def),
-    "segmentFilter_ga4"
+    my_segment,
+    "segmentDef_ga4"
   )
-  segment_ga4(
-    name = segment_name,
-    session_segment = segment_def
+  switch(
+    ScopeLevel(my_segment),
+    sessions = segment_ga4(
+      name = segment_name,
+      session_segment = segment_def
+    ),
+    users = segment_ga4(
+      name = segment_name,
+      user_segment = segment_def
+    )
   )
 })
+my_segment_list <- do.call(c, my_segment_list)
 
 google_analytics(
   viewId = 157157785,
