a71f158ae0b3927a6665ff9cad36553586d68871
Johann de Boer <johanndeboer@gmail.com>
2018-06-03 18:28:56 +1000

    Improve demo of googleAnalyticsR integration

--- demo/googleAnalyticsR_demo.R
+++ demo/googleAnalyticsR_demo.R
@@ -1,23 +1,29 @@
 library(ganalytics)
 library(googleAnalyticsR)
 
+my_dim_table_filter <- Expr(~medium == "organic") & Expr(~source == "google")
+
 my_segment_list <- list(
-    bounced_sessions = PerSession(Expr(~bounces != 0)),
-    from_google = Expr(~source == "google"),
-    repeat_user_sessions = Include(PerUser(Expr(~sessions > 1)), scope = "users")
-  )
-
-my_segment_list <- as(Segments(my_segment_list), "segment_ga4")
-
-my_dim_table_filter <- Expr(~medium == "(none)")
-my_dim_table_filter <- as(TableFilter(my_dim_table_filter), ".filter_clauses_ga4")
-
-google_analytics(
-  viewId = 157157785,
-  date_range = c("7daysAgo", "yesterday"),
-  metrics = c("users", "sessions"),
-  dimensions = c("segment", "medium"),
-  segments = my_segment_list,
-  dim_filters = my_dim_table_filter
+  bounced_sessions = PerSession(Expr(~bounces != 0)),
+  mobile_or_table = Expr(~deviceCategory %in% c("mobile", "tablet")),
+  converters = PerUser(Expr(~goalCompletionsAll > 0) | Expr(~transactions > 0)),
+  repeat_user_sessions = Include(PerUser(Expr(~sessions > 1)), scope = "users"),
+  new_desktop_users = Expr(~deviceCategory == "desktop") & Expr(~userType == "new")
 )
 
+segment_chunks <- split(my_segment_list, (seq_along(my_segment_list) - 1) %/% 4)
+
+results <- lapply(segment_chunks, function(chunk) {
+  google_analytics(
+    viewId = 157157785,
+    date_range = c("7daysAgo", "yesterday"),
+    metrics = c("users", "sessions"),
+    dimensions = c("segment", "medium"),
+    segments = Segments(chunk),
+    dim_filters = TableFilter(my_dim_table_filter)
+  )
+})
+
+results <- do.call(rbind, results)
+rownames(results) <- NULL
+results
