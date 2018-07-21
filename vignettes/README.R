51d22133b7d9af8825e2e4d3ff16af48d28582bb
Johann de Boer <johanndeboer@gmail.com>
2018-07-21 11:24:51 +1000

    Try inserting image again

--- vignettes/README.R
+++ vignettes/README.R
@@ -5,6 +5,11 @@ knitr::opts_chunk$set(
   fig.path = "README-"
 )
 
+## ----echo = FALSE--------------------------------------------------------
+knitr::include_graphics(
+  devtools::package_file("inst", "figures", "hexicon.png")
+)
+
 ## ------------------------------------------------------------------------
 Sys.setenv(
   GOOGLE_APIS_CONSUMER_ID = "<Your client ID>",
