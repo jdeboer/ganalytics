4e7f4472b1c77c2b85e3074f0b9f85f882f76f67
Johann de Boer <johanndeboer@gmail.com>
2018-07-22 17:52:15 +1000

    Reintroduce coercion to googleAnalyticsR segments and segment lists.

--- R/googleAnalyticsR-coerce.R
+++ R/googleAnalyticsR-coerce.R
@@ -208,32 +208,32 @@ setAs("gaSegmentSequenceFilter", "segmentDef_ga4", def = function(from, to) {
   as(as(from, "gaDynSegment"), to)
 })
 
-# setAs("gaDynSegment", "dynamicSegment_ga4", def = function(from, to) {
-#   dyn_segment <- list(
-#     name = from@name,
-#     userSegment = as(select_segment_filters_with_scope(from, scope = "users"), "segmentDef_ga4"),
-#     sessionSegment = as(select_segment_filters_with_scope(from, scope = "sessions"), "segmentDef_ga4")
-#   )
-#   class(dyn_segment) <- "dynamicSegment_ga4"
-#   dyn_segment
-# })
-#
-# setAs("gaSegmentList", "segment_ga4", def = function(from, to) {
-#   segment_list <- lapply(seq_along(from), function(segment_i) {
-#     segment_name <- names(from)[segment_i]
-#     if(is.null(segment_name)) segment_name <- character(0)
-#     segment <- from[[segment_i]]
-#     switch (class(segment),
-#       gaDynSegment = {
-#         segment@name = segment_name
-#         list(dynamicSegment = as(segment, "dynamicSegment_ga4"))
-#       },
-#       gaSegmentId = list(segmentId = as(segment, "character"))
-#     )
-#   })
-#   class(segment_list) <- to
-#   segment_list
-# })
+setAs("gaDynSegment", "dynamicSegment_ga4", def = function(from, to) {
+  dyn_segment <- list(
+    name = from@name,
+    userSegment = as(select_segment_filters_with_scope(from, scope = "users"), "segmentDef_ga4"),
+    sessionSegment = as(select_segment_filters_with_scope(from, scope = "sessions"), "segmentDef_ga4")
+  )
+  class(dyn_segment) <- "dynamicSegment_ga4"
+  dyn_segment
+})
+
+setAs("gaSegmentList", "segment_ga4", def = function(from, to) {
+  segment_list <- lapply(seq_along(from), function(segment_i) {
+    segment_name <- names(from)[segment_i]
+    if(is.null(segment_name)) segment_name <- character(0)
+    segment <- from[[segment_i]]
+    switch (class(segment),
+      gaDynSegment = {
+        segment@name = segment_name
+        list(dynamicSegment = as(segment, "dynamicSegment_ga4"))
+      },
+      gaSegmentId = list(segmentId = as(segment, "character"))
+    )
+  })
+  class(segment_list) <- to
+  segment_list
+})
 
 setAs(".compoundExpr", "segmentDef_ga4", def = function(from, to) {
   as(as(from, "gaDynSegment"), to)
